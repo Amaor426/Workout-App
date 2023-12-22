b2791b1ac6b96466573ff23e457752b85dbf2276
Amaor426 <150510076+Amaor426@users.noreply.github.com>
2023-12-21 16:52:07 -0800

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/styles.css';
+import WorkoutList from './components/WorkoutList';
+
+
+const App = () => {
+  return (
+    <div>
+      <WorkoutList />
+    </div>
+  );
+};
+
+export default App;
